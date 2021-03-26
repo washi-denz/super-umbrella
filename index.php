9484fb3050c8cf37a51e90bdb027ace67687b85a
washi-denz <washidenz@gmail.com>
2021-03-26 12:40:46 -0500

    Meros cambios XD

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
-<?php
-echo "Hola mundo";
-echo "Hola universo";
-echo "De nuevo...";
+<div 
+    style="
+    background:#fef9e7;
+    border:1px solid #f9e79f;
+    color:#34495e; 
+    font-weight:600;
+    text-align:center;
+    font-family:sans-serif;
+    padding:0.5rem;
+    "
+ >Hola universo</div>
